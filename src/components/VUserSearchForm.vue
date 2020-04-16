aca1190b837b2a6c3dd67ea597bbea5281523eb5
Hardik Shah <hardik.4546@gmail.com>
2020-04-16 18:25:58 +0530

    text changes

--- src/components/VUserSearchForm.vue
+++ src/components/VUserSearchForm.vue
@@ -1,7 +1,8 @@
 <template>
   <form @submit.prevent="$emit('submitted', username)">
-    <input type="text" v-model="username" />
-    <button type="submit">Enviar</button>
+    <input type="text" v-model="username" placeholder="Search for github user profile via github username" />
+    <button type="submit">Submit</button>
+    <div style="font-size: 12px;">(Search for github user profile via github username)</div>
   </form>
 </template>
 
